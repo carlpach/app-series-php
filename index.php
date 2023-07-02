9a6b4a8c4faeb6f892b00ed0248f88ab9b4e1e80
carlos <carlos.pacheco@bootcamp-upgrade.com>
2023-07-02 19:43:58 +0200

    styles

--- index.php
+++ index.php
@@ -12,26 +12,23 @@
     $serieFriends = new Serie("Friends", "Comedy");
     $tempUno = new Temporada(1, 2018);
 
-
-    require_once "index.html";
-
-
-    echo $_POST["number"];
-    echo gettype($_POST["number"]);
-    echo is_string($_POST["number"]);
-    $cap1 = new Capitulo($_POST["titulo"], new DateTime($_POST["fecha"]), floatval($_POST["valoracion"]), intval($_POST["number"]), intval($_POST["duration"]));
-
-        
-    $number = $_POST["fecha"];
-    echo $cap1->fecha->format('d F, Y');
-
     $serieFriends->addTemporada($tempUno);
-    echo "<h2>temporada de la serie $serieFriends->name</h2>";
+    echo "<h1>Serie $serieFriends->name</h1>";
+    echo "<h3>Temporadas</h3>";
     foreach ($serieFriends->getTemporadas() as $key => $temp) {
         echo "<p># $temp->number</p>";
         echo "<p>year $temp->year</p>";
 
     }
+
+    require_once "index.html";
+
+    $cap1 = new Capitulo($_POST["titulo"], new DateTime($_POST["fecha"]), floatval($_POST["valoracion"]), intval($_POST["number"]), intval($_POST["duration"]));
+
+        
+    $number = $_POST["fecha"];
+
+
 
     $tempUno->addCapitulo($cap1);
 
@@ -39,13 +36,18 @@
     $allCapitulos = getCapitulos($tempUno);
 
 
-    echo "<h3>Capitulos de la temporada $tempUno->number de la serie $serieFriends->name</h3>";
+    echo "<h3 class='b-section' >Capitulos de la temporada $tempUno->number de la serie $serieFriends->name</h3>";
 
+    echo "<div class='capitulos'>";
     foreach ($allCapitulos as $key => $capi) {
-        echo "<p>$capi->titulo</p>";
-        $fechaTest = $capi->fecha->format('d F, Y');
-        echo "<p>$fechaTest</p>";
-        echo "<p>valoracio'n $capi->valoracion</p>";
+        echo "<div class='capitulos__item'>";
+            echo "<p>$capi->titulo</p>";
+            $fechaTest = $capi->fecha->format('d F, Y');
+            echo "<p>$fechaTest</p>";
+            echo "<p>valoracio'n $capi->valoracion</p>";
+        echo "</div>";
 
     }
+    echo "</div>";
+
 
